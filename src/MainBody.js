57a336b923a7247ff39e70090765635461e5d782
Sona Sargsyan <sargsyan.sona1.y@gmail.com>
2024-07-09 09:36:35 -0700

    finalProject

--- src/MainBody.js
+++ src/MainBody.js
@@ -20,25 +20,24 @@ const Div = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   padding: theme.spacing(1),
 }));
-let count = 0;
 
 
-function MainBody({ score, setScore, points, setPoints, showType }) {
+
+function MainBody({ score, setScore, points, setPoints }) {
+  // Trivia API
   const [bestScore, setBestScore] = useState(0);
-  let type = "11";
   const [currentQuestion, setCurrentQuestion] = useState("")
-  const [showQuestion, setShowQuestion] = useState(false)
-  const [category, setCategory] = useState("9")
-
+  const [category, setCategory] = useState("")
+  const [correctAnswer, setCorrectAnswer] = useState("");
   //9 --> general knowledge
 
+
+  // Movie API
   let generateRandomMovie = () => {
     let num = Math.floor(Math.random() * 300);
     let movie = movies.popular_movies[num].movie_name;
-    count++;
     return movie;
   };
-
   const [poster, setPoster] = useState("");
   const [currentActors, setCurrentActors] = useState([]);
   const [chosenActor, setChosenActor] = useState("");
@@ -63,7 +62,6 @@ function MainBody({ score, setScore, points, setPoints, showType }) {
     };
 
     let randomMovie = generateRandomMovie();
-
     fetch(`http://www.omdbapi.com/?t=${randomMovie}&apikey=${apiKey}`, requestOptions)
       .then((response) => response.json())
       .then((result) => {
@@ -75,33 +73,34 @@ function MainBody({ score, setScore, points, setPoints, showType }) {
       });
   }
 
-  function fetchRandomQuestion(){
+  function fetchRandomQuestion() {
     const requestOptions = {
       method: "GET",
     };
-
-    fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=boolean`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        setCurrentQuestion(result.results[0].question);
-        // setShowQuestion(true)
-        // alert("You answered 2 questions correct in a row.\nTime for a quick game which can earn you 200 POINTS!!\nNo points are reduced if you answer wrong");
-      })
-      .catch((error) => {
-        console.error(error)
-      });
+    if (category !== "") {
+      fetch(`https://opentdb.com/api.php?amount=1&category=${category}&type=boolean`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          setCorrectAnswer(result.results[0].correct_answer);
+          setCurrentQuestion(result.results[0].question);
+        })
+        .catch((error) => {
+          console.error(error)
+        });
+    }
   }
 
- 
+
   function handleEnterPress() {
     if (checkActor(currentActors, chosenActor)) {
       alert("Correct!");
       setChosenActor("");
       setScore(score + 1);
       setPoints(points + 100);
-      setBestScore(bestScore+1)
+      setBestScore(bestScore + 1);
       fetchMovie();
-      if(!poster){
+      if (!poster) {
         fetchMovie();
       }
     }
@@ -112,67 +111,64 @@ function MainBody({ score, setScore, points, setPoints, showType }) {
       setChosenActor("");
       setScore(0);
       setPoints(0);
+      setBestScore(0);
     }
 
   }
 
-  function skipQuestion(){
-    if(points>=100){
-      setPoints(points-100)
+  function skipQuestion() {
+    if (points >= 100) {
+      setPoints(points - 100)
       setChosenActor("");
       setPoster("");
       fetchMovie();
-      if(!poster){
+      if (!poster) {
         fetchMovie();
       }
     }
-    else{
+    else {
       alert("You have to have at least 100 points! Can't skip :( \n")
     }
   }
 
-  function getHint(){
-    if(points>=50){
-      setPoints(points-50)
+  function getHint() {
+    if (points >= 50) {
+      setPoints(points - 50)
       console.log(currentActors)
       let currMovieActor = currentActors[0].split(' ')[0]
       alert(`The first name of the actor is: ${currMovieActor} \n -50 points`)
     }
-    else{
+    else {
       alert("You have to have at least 50 points! No hints :(")
     }
   }
 
 
   const handleAnswer = (answer) => {
-    if(answer){
+    if (answer === correctAnswer) {
       alert("Correct! +200 points");
-      setPoints(points+200)
+      setPoints(points + 200)
     }
-    else{
+    else {
       alert("Incorrect! No points added.")
     }
-    // setShowQuestion(false)
+    setCategory("");
+    setCorrectAnswer("");
+    setCurrentQuestion("");
     setBestScore(0)
-    
   }
 
-  // useEffect(() => {
-  //   if(bestScore>=2){
-  //     fetchRandomQuestion();
-  //   }
-  // }, [bestScore])
-
-  useEffect(()=>{
-    if(!poster){
+  useEffect(() => {
+    if (!poster) {
       fetchMovie();
     }
   })
 
-  // if(!poster){
-  //   fetchMovie();
-  // }
-
+  useEffect(() => {
+    if (category !== "") {
+      fetchRandomQuestion();
+    }
+  }, [category]);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -180,54 +176,53 @@ function MainBody({ score, setScore, points, setPoints, showType }) {
   };
   const handleClose = () => {
     setAnchorEl(null);
-
   };
 
   const handleCategoryChange = (selectedCategory) => {
     setAnchorEl(null);
-    setCategory(selectedCategory)
-    fetchRandomQuestion();
+    setCategory(selectedCategory);
   }
 
 
-
-
-  if (bestScore>=2) {
+  if (bestScore % 2 === 0 && bestScore !== 0) {
     return (
       <div className="MainBody">
         <Div className="question-component">
+          <Typography className = "win-text">(You answered 2 questions correct in a row)</Typography>
+          <Typography className = "text choose-category">Choose the category of the question</Typography>
 
-          <Typography>Choose the category of the question</Typography>
+          <Button className = "category-button"
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            Choose Category
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={() => handleCategoryChange("9")}>General Knowledge</MenuItem>
+            <MenuItem onClick={() => handleCategoryChange("10")}>Books</MenuItem>
+            <MenuItem onClick={() => handleCategoryChange("11")}>Movies</MenuItem>
+            <MenuItem onClick={() => handleCategoryChange("12")}>Music</MenuItem>
+            <MenuItem onClick={() => handleCategoryChange("15")}>Video Games</MenuItem>
+            <MenuItem onClick={() => handleCategoryChange("22")}>Geography</MenuItem>
+          </Menu>
 
-          <Button
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-        >
-          Dashboard
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          <MenuItem onClick={()=> handleCategoryChange("9")}>General Knowledge</MenuItem>
-          <MenuItem onClick={()=> handleCategoryChange("10")}>Books</MenuItem>
-          <MenuItem onClick={()=> handleCategoryChange("11")}>Movies</MenuItem>
-          <MenuItem onClick={()=> handleCategoryChange("12")}>Music</MenuItem>
-          <MenuItem onClick={()=> handleCategoryChange("10")}>Video Games</MenuItem>
-          <MenuItem onClick={()=> handleCategoryChange("10")}>Geography</MenuItem>
-        </Menu>
-
-          <Typography>{currentQuestion}</Typography>
-          <Button variant="outlined" onClick={() => handleAnswer(true)}>True</Button>
-          <Button variant="outlined" onClick={() => handleAnswer(false)}>False</Button>
+          <Typography className = "question">{currentQuestion}</Typography>
+          <Div className='answer-buttons'>
+            <Button className = "true-false" variant="outlined" onClick={() => handleAnswer("True")}>True</Button>
+            <Button className = "true-false" variant="outlined" onClick={() => handleAnswer("False")}>False</Button>
+          </Div>
+          
         </Div>
       </div>
     );
@@ -239,12 +234,12 @@ function MainBody({ score, setScore, points, setPoints, showType }) {
         <img src={poster} alt=""></img>
       </Div>
       <Div className='play-portion'>
-        <Typography className = "text">Write down the actor's name below</Typography>
-        <TextField className = "input" value={chosenActor} onChange={handleInputChange}
+        <Typography className="text">Write down the actor's name below</Typography>
+        <TextField className="input" value={chosenActor} onChange={handleInputChange}
           id="movieSearch" label="enter the name" variant="outlined" />
         <Button className='enter button' onClick={handleEnterPress} variant="outlined">enter</Button>
-        <Button className = "skip button" variant="outlined" onClick={skipQuestion}>Skip<SkipNextIcon/></Button>
-        <Button className = "hint button" variant="outlined" onClick = {getHint}>Hint<TipsAndUpdatesIcon/>
+        <Button className="skip button" variant="outlined" onClick={skipQuestion}>Skip<SkipNextIcon /></Button>
+        <Button className="hint button" variant="outlined" onClick={getHint}>Hint<TipsAndUpdatesIcon />
         </Button>
 
       </Div>
